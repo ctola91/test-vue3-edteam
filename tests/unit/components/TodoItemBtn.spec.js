c2bc4ea1d3e06d30b37e25b16bb1a296de93871f
ctola91 <ctolapacheco@gmail.com>
2022-02-04 00:07:36 -0400

    TodoItemBtn tests

---
+++ tests/unit/components/TodoItemBtn.spec.js
@@ -0,0 +1,34 @@
+import { mount } from "@vue/test-utils";
+import TodoItemBtn from "@/components/TodoItemBtn.vue";
+
+describe("TodoItemBtn.vue", () => {
+  it("El boton se carga sin pasarle un slot", () => {
+    const wrapper = mount(TodoItemBtn, {
+      props: {
+        task: {
+          id: 1,
+          task: "Probar que funciona mi botom",
+          isCompleted: false,
+        },
+      },
+    });
+    // expect(wrapper.find("span").text()).toBe("Button");
+    expect(wrapper.html()).toContain("Button");
+  });
+
+  it("El boton se carga con un slot", () => {
+    const wrapper = mount(TodoItemBtn, {
+      props: {
+        task: {
+          id: 1,
+          task: "Probar que funciona mi botom",
+          isCompleted: false,
+        },
+      },
+      slots: {
+        default: "<div>Aceptar</div>",
+      },
+    });
+    expect(wrapper.html()).toContain("Aceptar");
+  });
+});
